1754f35c331fe949361d13cfa81c565fac8cbd98
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-06-15 16:03:52 -0300

    feat: implement the uniform grid algorithm

--- main.py
+++ main.py
@@ -7,6 +7,28 @@ from pygame.math import Vector2
 
 particles = []
 WIDTH, HEIGHT = 800, 800
+grid = []
+grid_size = 50
+
+def add_particle_to_grid(particle):
+    i, j = get_particle_position_on_grid(particle)
+    grid[i][j].append(particle)
+
+def get_particle_position_on_grid(particle):
+    x = particle.pos.x
+    y = particle.pos.y
+    i = int(x//grid_size)-1 if x > 0 else 0
+    j = int(y//grid_size)-1 if y > 0 else 0
+    return (i,j)
+    
+def clear_grid():
+    grid.clear()
+    rows = HEIGHT//grid_size 
+    columns = WIDTH//grid_size 
+    for i in range(rows):
+        grid.append([])
+        for j in range(columns):
+            grid[i].append([])
 
 def add_particle(position, direction, speed, radius, color):
     particles.append(Particle(position, direction, speed, radius, color))
@@ -20,15 +42,17 @@ def set_particles_speed(speed):
 def draw_particles():
     for particle in particles:
         particle.draw(screen)
-        particle.guidance([0, WIDTH, 0, HEIGHT], particles)
         particle.update_pos()
+        add_particle_to_grid(particle)
+        i,j = get_particle_position_on_grid(particle)
+        particle.guidance([0, WIDTH, 0, HEIGHT], grid[i][j])
 
 def create_particles():
-    for i in range(2000):
+    for i in range(5000):
         pos = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
         angle = random.uniform(0, 2 * math.pi)  
         dir = Vector2(math.cos(angle), math.sin(angle))
-        speed = 3
+        speed = 2
         radius = 2
         add_particle(pos, dir, speed, radius, (255, 255, 255))
 
@@ -45,6 +69,7 @@ def render():
                     running = False
 
         screen.blit(bg, (0, 0))
+        clear_grid()
         draw_particles()
         clock.tick(30)
         pygame.display.update()
